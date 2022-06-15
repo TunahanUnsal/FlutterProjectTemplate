ff297512d9e74bf5fe7b847d455b7f228cf50c8b
Tunahan Ünsal <tunahan.unsal@eczacibasi.com.tr>
2022-06-15 17:04:29 +0300

    first commit <3

---
+++ android/app/src/main/kotlin/com/vitra/remote/vitra_smart_remote/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vitra.remote.vitra_smart_remote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
